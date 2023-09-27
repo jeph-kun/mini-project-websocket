2c15f29a10deb1b8a08901b51a05ddc49734818b
Dulitha K <dulitha.kumarasiri@emapta.com>
2023-09-28 01:14:40 +0530

    Update MeetingInner.jsx

--- src/pages/MeetingInner.jsx
+++ src/pages/MeetingInner.jsx
@@ -34,15 +34,15 @@ export default function MeetingInner() {
             pb: 6,
           }}
         >
-          <Container 
+          <Container
             maxWidth="sm"
             sx={{
               pt: 1,
               pb: 1,
             }}
-            >
+          >
             <Typography
-              component="h1"
+              component="h4"
               variant="h4"
               align="left"
               color="text.primary"
@@ -52,53 +52,52 @@ export default function MeetingInner() {
             </Typography>
           </Container>
 
-          <Container 
-            sx={{ 
-              py: 8, 
-              backgroundColor:"#fff",
-              borderRadius:"6px",  
-            }} 
-              maxWidth="md"
-            >
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{
-                    height: "100%",
-                    display: "flex",
-                    flexDirection: "column",
-                  }}
-                >
-                  <CardMedia
-                    component="div"
+          <Container
+            sx={{
+              py: 8,
+              backgroundColor: "#fff",
+              borderRadius: "10px",
+            }}
+            maxWidth="md"
+          >
+            {/* End hero unit */}
+            <Grid container spacing={4}>
+              {cards.map((card) => (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card
                     sx={{
-                      // 16:9
-                      pt: "56.25%",
+                      height: "100%",
+                      display: "flex",
+                      flexDirection: "column",
                     }}
-                    image="https://source.unsplash.com/random?wallpapers"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe
-                      the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
+                  >
+                    <CardMedia
+                      component="div"
+                      sx={{
+                        // 16:9
+                        pt: "56.25%",
+                      }}
+                      image="https://source.unsplash.com/random?wallpapers"
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        Heading
+                      </Typography>
+                      <Typography>
+                        This is a media card. You can use this section to
+                        describe the content.
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">View</Button>
+                      <Button size="small">Edit</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
         </Box>
-        
       </main>
     </ThemeProvider>
   );
